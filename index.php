eb0ac0ba86091ffda50de6b656c6ec3b6ad17a69
Adhoul <adhoul1011@gmail.com>
2017-06-24 11:02:29 +0000

    added images folder and edit index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>BruActiv Comming Soon...</title>
+    </head>
+    <body>
+        <center>
+            <div>
+                <img src="images/RAMADHAN-Logo.gif"style="width:60%;">
+            </div>
+            <div>
+                <img src="images/ZACK_PEEKING.gif"style="width:15%;">
+            </div>
+        </center>
+    </body>
+</html>
